d8d401eda81f6e27e22a88881e89cbed5bf54f60
MartaPoti <poti.pepto.bismol@gmail.com>
2023-03-15 16:41:03 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
      hello
+<button className='btn btn-primary'>jo</button>
+     <footer>This project was coded by {" "} Marta Potulska and is opensourced on {" "} <a href="https://github.com/MartaPoti/react-weather-app"> GitHub</a>
+     </footer>
+
     </div>
   );
 }
 
-export default App;
+ 
