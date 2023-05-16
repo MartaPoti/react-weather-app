85fd6b138bb4d2f16bc98452742750dab9ddf248
MartaPoti <poti.pepto.bismol@gmail.com>
2023-05-16 16:37:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
     <div className="container">
-     <Weather defaultCity="Tokyo"/>
+     <Weather defaultCity="Heilbronn"/>
     <footer>This project was coded by {" "} Marta Potulska and is opensourced on {" "} <a href="https://github.com/MartaPoti/react-weather-app"> GitHub</a>
     </footer>
     </div>
